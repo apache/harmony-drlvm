b28f5f3e4024aa64742be87deab8fb7a02eeceb3
Geir Magnusson Jr <geirm@apache.org>
2006-10-07 18:31:20 +0000

    HARMONY-1626
    additional file I forgot to commit
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/drlvm/trunk@453981 13f79535-47bb-0310-9956-ffa450edef68

---
+++ vm/vmcore/include/classpath_const.h
@@ -0,0 +1,37 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#ifndef CLASSPATH_CONST_H_
+#define CLASSPATH_CONST_H_
+
+/**
+ *   Constants used for classpath definition and manipulation
+ */
+
+#define XBOOTCLASSPATH         "-Xbootclasspath:"
+#define XBOOTCLASSPATH_A       "-Xbootclasspath/a:"
+#define XBOOTCLASSPATH_P       "-Xbootclasspath/p:"
+
+#define O_A_H_BOOT_CLASS_PATH  "org.apache.harmony.boot.class.path"
+#define O_A_H_VM_VMDIR         "org.apache.harmony.vm.vmdir"
+
+#define KERNEL_JAR             "kernel.jar"
+
+#define VM_BOOT_CLASS_PATH     "vm.boot.class.path"
+#define SUN_BOOT_CLASS_PATH    "sun.boot.class.path"
+
+#endif /*CLASSPATH_CONST_H_*/
+
