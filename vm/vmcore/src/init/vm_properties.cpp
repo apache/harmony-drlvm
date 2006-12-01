apache/harmony-drlvm

commit f31e2d9e0dd8827254263ba8fac294b3c90cc9ee

HARMONY-1925 [drlvm] VM property module should be redeveloped. VM properties initialization should be improved. Substantially redone the original patch. Tested on SUSE9, Windows 2003 Server, WinXP.
git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@480913 13f79535-47bb-0310-9956-ffa450edef68

vm/vmcore/src/init/vm_properties.cpp
@@ -1,0 +1,380 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <apr_file_io.h>
+#include <apr_file_info.h>
+#include <apr_env.h>
+#include <apr_strings.h>
+#include "port_dso.h"
+#include "port_filepath.h"
+#include "port_sysinfo.h"
+
+#define LOG_DOMAIN "init.properties"
+#include "cxxlog.h"
+#include "properties.h"
+#include "vm_properties.h"
+#include "init.h"
+
+inline char* unquote(char *str)
+{
+    const char *tokens = " \t\n\r\'\"";
+    size_t i = strspn(str, tokens);
+    str += i;
+    char *p = str + strlen(str) - 1;
+    while(strchr(tokens, *p) && p >= str)
+        *(p--) = '\0';
+    return str;
+}
+
+#define PROP_ENV_NAME  "VM_PROPERTIES"
+#define PROP_FILE_NAME "vm.properties"
+
+#define MAX_PROP_LINE 5120
+
+// local memory pool for temporary allocation
+static apr_pool_t *prop_pool;
+
+static void
+read_properties(char *fname, Properties & prop)
+{
+    apr_file_t *f;
+    char *src, *tok;
+    if (APR_SUCCESS != apr_file_open(&f, fname, APR_FOPEN_READ, 0, prop_pool))
+        return;
+    WARN("USAGE OF vm.properties FILE IS DEPRECATED. THIS FUNCTIONALITY WILL BE REMOVED SOON. ALL PROPERTIES SHOULD BE SET ON COMMAND LINE");
+    char buf[MAX_PROP_LINE];
+    while (!apr_file_eof(f) && !apr_file_gets(buf, MAX_PROP_LINE, f))
+    {
+        if (buf[0] != '#') {
+            src = strdup(buf);
+            tok = strchr(src, '=');
+            if(tok)
+            {
+                *tok = '\0';
+                prop.set(unquote(src), unquote(tok + 1));
+            }
+            STD_FREE(src);
+        }
+    }
+    apr_file_close(f);
+}
+
+#define API_DLL1 "harmonyvm"
+#define API_DLL2 "hythr"
+#define API_DLL3 "hysig"
+#define API_DLL4 "hyprt"
+#define API_DLL5 "hyzlib"
+#define API_DLL6 "hytext"
+#define API_DLL7 "hynio"
+#define API_DLL8 "vmi"
+#define API_DLLA "hyluni"
+#define API_DLLB "hyarchive"
+
+#define GC_DLL "gc_cc"
+
+/**
+ *  Compose a string of file names each of them beginning with path,
+ *  names separated by PORT_PATH_SEPARATOR.  If patch is NULL, no path
+ *  or separator will be prefixed
+ */
+static char *compose_full_files_path_names_list(const char *path,
+                                                const char **dll_names,
+                                                const int names_number, 
+                                                bool is_dll)
+{
+    char* full_name = "";
+    for (int iii = 0; iii < names_number; iii++)
+    {
+        const char *tmp = dll_names[iii];
+        if (is_dll) {
+            tmp = port_dso_name_decorate(tmp, prop_pool);
+        }
+        
+        /*
+         *  if the path is non-null, prefix, otherwise do nothing
+         *  to avoid the problem of "/libfoo.so" when we don't want
+         *  a path attached
+         */
+         
+        if (path != NULL) { 
+            tmp = port_filepath_merge(path, tmp, prop_pool);
+        }
+        
+        full_name = apr_pstrcat(prop_pool, full_name, tmp, 
+            (iii + 1 < names_number) ? PORT_PATH_SEPARATOR_STR : "", NULL);
+    }
+
+    return full_name;
+}
+
+static void init_java_properties(Properties & properties)
+{
+    //java part
+    //!!! java.compiler property must be defined by EM
+
+    char *os_name, *os_version, *path;
+    const char *tmp;
+    char *path_buf = NULL;
+
+    port_OS_name_version(&os_name, &os_version, prop_pool);
+    apr_filepath_get(&path, APR_FILEPATH_NATIVE, prop_pool);
+    if (APR_SUCCESS != apr_temp_dir_get(&tmp, prop_pool)) {
+        tmp = ".";
+    }
+    properties.set("java.version", "1.5.0");
+    properties.set("java.vendor", "Apache Software Foundation");
+    properties.set("java.vendor.url", "http://harmony.apache.org");
+    // java.home initialization, try to find absolute location of the executable and set
+    // java.home to the parent directory.
+    char *base_path_buf;
+    if (port_executable_name(&base_path_buf, prop_pool) != APR_SUCCESS) {
+        DIE("Failed to find executable location");
+    }
+    // directory for the executable
+    char *p = strrchr(base_path_buf, PORT_FILE_SEPARATOR);
+    if (NULL == p)
+        DIE("Failed to determine executable parent directory");
+    *p = '\0';
+    properties.set("java.home", base_path_buf);
+    properties.set("java.vm.specification.version", "1.0");
+    properties.set("java.vm.specification.vendor", "Sun Microsystems Inc.");
+    properties.set("java.vm.specification.name", "Java Virtual Machine Specification");
+    properties.set("java.vm.version", "11.2.0");
+    properties.set("java.vm.vendor", "Apache Software Foundation");
+    properties.set("java.vm.name", "DRLVM");
+    properties.set("java.specification.version", "1.5");
+    properties.set("java.specification.vendor", "Sun Microsystems Inc.");
+    properties.set("java.specification.name", "Java Platform API Specification");
+    properties.set("java.class.version", "49.0");
+    properties.set("java.class.path", ".");
+
+    // java.library.path initialization, the value is the location of VM executable,
+    // prepended to OS library search path
+    char *env;
+    char *lib_path = base_path_buf;
+    if (APR_SUCCESS == port_dso_search_path(&env, prop_pool))
+    {
+        lib_path = apr_pstrcat(prop_pool, base_path_buf, PORT_PATH_SEPARATOR_STR,
+                               base_path_buf, PORT_FILE_SEPARATOR_STR, "default",
+                               PORT_PATH_SEPARATOR_STR, env, NULL);
+    }
+    properties.set("java.library.path", lib_path);
+    //java.ext.dirs initialization.
+    char *ext_path = port_filepath_merge(base_path_buf, "lib" PORT_FILE_SEPARATOR_STR "ext", prop_pool);
+    properties.set("java.ext.dirs", ext_path);
+    properties.set("os.name", os_name);
+    properties.set("os.arch", port_CPU_architecture());
+    properties.set("os.version", os_version);
+    properties.set("file.separator", PORT_FILE_SEPARATOR_STR);
+    properties.set("path.separator", PORT_PATH_SEPARATOR_STR);
+    properties.set("line.separator", APR_EOL_STR);
+    // user.name initialization, try to get the name from the system
+    char *user_buf;
+    apr_status_t status = port_user_name(&user_buf, prop_pool);
+    if (APR_SUCCESS != status) {
+        DIE("Failed to get user name from the system. Error code " << status);
+    }
+    properties.set("user.name", user_buf);
+    // user.home initialization, try to get home from the system.
+    char *user_home;
+    status = port_user_home(&user_home, prop_pool);
+    if (APR_SUCCESS != status) {
+        DIE("Failed to get user home from the system. Error code " << status);
+    }
+    properties.set("user.home", user_home);
+    // java.io.tmpdir initialization. 
+    const char *tmpdir;
+    status = apr_temp_dir_get(&tmpdir, prop_pool);
+    if (APR_SUCCESS != status) {
+        tmpdir = user_home;
+    }
+    properties.set("java.io.tmpdir", tmpdir);
+    properties.set("user.dir", path);
+
+    // FIXME??? other (not required by api specification) properties
+    
+    properties.set("java.vm.info", "no info");
+    properties.set("java.tmpdir", tmp);
+    properties.set("user.language", "en");
+    properties.set("user.region", "US");
+    properties.set("file.encoding", "8859_1");
+
+    // FIXME user.timezone initialization, required by java.util.TimeZone implementation
+    char *user_tz;
+    status = port_user_timezone(&user_tz, prop_pool);
+    if (APR_SUCCESS != status) {
+        INFO("Failed to get user timezone from the system. Error code " << status);
+        user_tz = "GMT";
+    }
+    
+    properties.set("user.timezone", user_tz);
+
+    // FIXME: This is a workaround code for third party APIs which depend on this property.
+    properties.set("java.util.prefs.PreferencesFactory",
+#ifdef PLATFORM_NT
+        "java.util.prefs.RegistryPreferencesFactoryImpl");
+#else
+        "java.util.prefs.FilePreferencesFactoryImpl");
+#endif
+
+    // Added for compatibility with the external java JDWP agent
+    properties.set("sun.boot.library.path", base_path_buf);
+
+    /*
+    *  it's possible someone forgot to set this property - set to default of .
+    */
+    if (!properties.is_set(O_A_H_VM_VMDIR)) {
+        TRACE2("init", "o.a.h.vm.vmdir not set - setting predefined value of as '.'");
+        properties.set(O_A_H_VM_VMDIR, ".");
+    }
+
+    /*
+    *  also, do the same for java.class.path
+    */
+    if (!properties.is_set("java.class.path")) {
+        TRACE2("init", "java.class.path not set - setting predefined value of as '.'");
+        properties.set("java.class.path", ".");
+    }
+}
+
+//vm part
+static void init_vm_properties(Properties & properties)
+{
+        properties.set("vm.assert_dialog", "true");
+        properties.set("vm.crash_handler", "false");
+        properties.set("vm.finalize", "true");
+        properties.set("vm.jit_may_inline_sync", "true");
+        properties.set("vm.use_interpreter", "false");
+        properties.set("vm.use_verifier", "true");
+        properties.set("vm.jvmti.enabled", "false");
+        properties.set("vm.cleanupOnExit", "false");
+        properties.set("vm.bootclasspath.appendclasspath", "false");
+
+        /*
+        *  vm.boot.library.path initialization, the value is the location of VM executable
+        * 
+        *  2006-09-06 gmj :  there's no reason to ever believe this is true given how the VM can be 
+        *  launched in a mariad of ways, so just set to empty string.
+        */
+        properties.set("vm.boot.library.path", "");
+
+        properties.set("vm.dlls", PORT_DSO_NAME(GC_DLL));
+
+        // vm.other_natives_dlls initialization, the value is location of VM executable.
+        const char *api_dll_files[] =
+        {
+            API_DLL1,
+                API_DLL2,
+                API_DLL3,
+                API_DLL4,
+                API_DLL5,
+                API_DLL6,
+                API_DLL7,
+                API_DLL8,
+                API_DLLA,
+                API_DLLB
+        };
+        int n_api_dll_files = sizeof(api_dll_files) / sizeof(char *);
+        /*
+        *  pass NULL for the pathname as we don't want 
+        *  any path pre-pended
+        */
+        char* path_buf = compose_full_files_path_names_list(NULL, api_dll_files, n_api_dll_files, true);
+        properties.set("vm.other_natives_dlls", path_buf);
+}
+
+void
+initialize_properties(Global_Env * p_env)
+{
+    if (!prop_pool) {
+        apr_pool_create(&prop_pool, 0);
+    }
+/*
+ * 0. Add predefined properties from property table
+ */
+     
+    init_java_properties(*p_env->JavaProperties());
+    init_vm_properties(*p_env->VmProperties());
+
+/* 
+ * 1. VM looks for an environment variable, say, 
+ *    VM_PROPERTIES=d:\xyz\eee\vm.Properties, read the Properties;
+ */
+    char *pf;
+    if (apr_env_get(&pf, PROP_ENV_NAME, prop_pool) == APR_SUCCESS){
+        read_properties(pf, *p_env->JavaProperties());
+    }
+/*
+ * 2. Looks for vm.Properties in the directory where vm executable resides 
+ *    (it's also a global file), read the Properties, if key is duplicated, 
+ *    override the value;
+ */
+    char *buf;
+    if (port_executable_name(&buf, prop_pool) == APR_SUCCESS)
+    {
+        char *p = strrchr(buf, PORT_FILE_SEPARATOR);
+        if (p)
+        {
+            *(p + 1) = '\0';
+            buf = apr_pstrcat(prop_pool, buf, PROP_FILE_NAME, NULL);
+            read_properties(buf, *p_env->JavaProperties());
+        }
+    }
+
+/*
+ * 3. Looks for it in current directory(it's an app-specific file), read the 
+ *    Properties, if key is duplicated, override the value;
+ */
+    apr_filepath_get(&buf, 0, prop_pool);
+    buf = port_filepath_merge(buf, PROP_FILE_NAME, prop_pool);
+    read_properties(buf, *p_env->JavaProperties());
+/*
+ * 4. Check whether there is a command line option, say, 
+ *    -Properties-file "d:\xyz\eee\vm.Properties" or -Droperties key=value, 
+ *    read the Properties, if key is duplicated, override the value. 
+ */
+    char *src, *tok;
+    for (int arg_num = 0; arg_num < p_env->vm_arguments.nOptions; arg_num++)
+    {
+        char *option = p_env->vm_arguments.options[arg_num].optionString;
+        if (strncmp(option, "-D", 2) == 0)
+        {
+            TRACE("setting property " << option + 2);
+            src = strdup(option + 2);
+            tok = strchr(src, '=');
+            if(tok)
+            {
+                *tok = '\0';
+                p_env->JavaProperties()->set(unquote(src), unquote(tok + 1));
+            }
+            STD_FREE(src);
+        } 
+        else if (strncmp(option, "-XD", 3) == 0)
+        {
+            TRACE("setting internal property " << option + 3);
+            src = strdup(option + 3);
+            tok = strchr(src, '=');
+            if(tok)
+            {
+                *tok = '\0';
+                p_env->VmProperties()->set(unquote(src), unquote(tok + 1));
+            }
+            STD_FREE(src);
+        }
+    }
+    apr_pool_clear(prop_pool);
+}

build/custom/msvc_2003/vmcore/vmcore.vcproj
@@ -508,6 +508,9 @@ copy $(OutDir)\harmonyvm.lib $(ProjectDir)\..\..\..\win_ia32_msvc_$(Configuratio
 				RelativePath="..\..\..\..\vm\vmcore\src\init\vm_main.cpp">
 			</File>
 			<File
+				RelativePath="..\..\..\..\vm\vmcore\src\init\vm_properties.cpp">
+			</File>
+			<File
 				RelativePath="..\..\..\..\vm\vmcore\src\init\vm_shutdown.cpp">
 			</File>
 		</Filter>

vm/em/src/DrlEMImpl.cpp
@@ -174,7 +174,9 @@ void DrlEMImpl::deallocateResources() {
  *  deprecated
  */
 std::string buildDefaultLibPath(const std::string& dll_name) {
-    std::string library_path = vm_get_property_value("vm.boot.library.path");
+    char* c_string_tmp_value = get_property("vm.boot.library.path", VM_PROPERTIES);
+    std::string library_path = c_string_tmp_value == NULL ? "" : c_string_tmp_value;
+    destroy_property_value(c_string_tmp_value);
 #ifdef PLATFORM_NT
     std::string fullPath = dll_name + ".dll";
     
@@ -214,7 +216,9 @@ std::string prepareLibPath(const std::string& origPath) {
 
     std::string path = origPath;
     if (path.find('/') == path.npos && path.find('\\') == path.npos ) {
-        std::string dir = vm_get_property_value(O_A_H_VM_VMDIR);
+        char* c_string_tmp_value = get_property(O_A_H_VM_VMDIR, JAVA_PROPERTIES);
+        std::string dir = c_string_tmp_value == NULL ? "" : c_string_tmp_value;
+        destroy_property_value(c_string_tmp_value);
         if (libPrefix.length() > 0 && !startsWith(path, libPrefix)) {
             path = libPrefix + path;
         }
@@ -297,9 +301,15 @@ static std::string readFile(const std::string& fileName) {
             } else if (startsWith(line, "-D") && (idx = line.find('=')) != std::string::npos) {
                 std::string name = line.substr(2, idx-2);                   
                 std::string value = line.substr(idx+1);
-                const char* old_value = vm_get_property_value(name.c_str());
-                if (old_value == NULL || *old_value == 0) {
-                    vm_properties_set_value(name.c_str(), value.c_str());
+                if (!is_property_set(name.c_str(), JAVA_PROPERTIES)) {
+                    set_property(name.c_str(), value.c_str(), JAVA_PROPERTIES);
+                }
+                continue;
+            } else if (startsWith(line, "-XD") && (idx = line.find('=')) != std::string::npos) {
+                std::string name = line.substr(3, idx-3);                   
+                std::string value = line.substr(idx+1);
+                if (!is_property_set(name.c_str(),VM_PROPERTIES)) {
+                    set_property(name.c_str(), value.c_str(), VM_PROPERTIES);
                 }
                 continue;
             } 
@@ -315,10 +325,12 @@ static std::string readFile(const std::string& fileName) {
 }
 
 std::string DrlEMImpl::readConfiguration() {
-    std::string  configFileName = vm_get_property_value("em.properties");
+    char* c_string_tmp_value = get_property("em.properties", VM_PROPERTIES);
+    std::string  configFileName = c_string_tmp_value == NULL ? "" : c_string_tmp_value;
+    destroy_property_value(c_string_tmp_value);
     if (configFileName.empty()) {
-        bool jitTiMode = vm_get_property_value_boolean("vm.jvmti.enabled", false);
-        bool interpreterMode = vm_get_boolean_property_value_with_default("vm.use_interpreter");
+        bool jitTiMode = get_boolean_property("vm.jvmti.enabled", FALSE, VM_PROPERTIES);
+        bool interpreterMode = get_boolean_property("vm.use_interpreter", FALSE, VM_PROPERTIES);
         configFileName = interpreterMode ? "interpreter" : (jitTiMode ? "ti" : "client");
     } 
     if (!endsWith(configFileName, EM_CONFIG_EXT)) {
@@ -326,7 +338,9 @@ std::string DrlEMImpl::readConfiguration() {
     }
 
     if (configFileName.find('/') == configFileName.npos && configFileName.find('\\') == configFileName.npos ) {
-        std::string defaultConfigDir = vm_get_property_value(O_A_H_VM_VMDIR);
+        c_string_tmp_value = get_property(O_A_H_VM_VMDIR, JAVA_PROPERTIES);
+        std::string defaultConfigDir = c_string_tmp_value == NULL ? "" : c_string_tmp_value;
+        destroy_property_value(c_string_tmp_value);
         
         configFileName = defaultConfigDir + "/" + configFileName;
     }
@@ -378,9 +392,13 @@ bool DrlEMImpl::initJIT(const std::string& libName, apr_dso_handle_t* libHandle,
 
 std::string DrlEMImpl::getJITLibFromCmdLine(const std::string& jitName) const {
     std::string propName = std::string("em.")+jitName+".jitPath";
-    std::string jitLib  = vm_get_property_value(propName.c_str());
+    char* c_string_tmp_value = get_property(propName.c_str(), VM_PROPERTIES);
+    std::string jitLib  = c_string_tmp_value == NULL ? "" : c_string_tmp_value;
+    destroy_property_value(c_string_tmp_value);
     if (jitLib.empty()) {
-        jitLib = vm_get_property_value("em.jitPath");
+        c_string_tmp_value = get_property("em.jitPath", VM_PROPERTIES);
+        jitLib = c_string_tmp_value == NULL ? "" : c_string_tmp_value;
+        destroy_property_value(c_string_tmp_value);
     }
     return jitLib;
 }

vm/gc_cc/src/init.cpp
@@ -65,50 +65,33 @@ bool remember_root_set = false;
 const char *lp_hint = NULL;
 bool jvmti_heap_iteration = false;
 
-static size_t parse_size_string(const char* size_string) {
-    size_t len = strlen(size_string);
-    size_t unit = 1;
-    if (tolower(size_string[len - 1]) == 'k') {
-        unit = 1024;
-    } else if (tolower(size_string[len - 1]) == 'm') {
-        unit = 1024 * 1024;
-    } else if (tolower(size_string[len - 1]) == 'g') {
-        unit = 1024 * 1024 * 1024;
-    }
+static size_t get_size_property(const char* name) 
+{
+    char* size_string = get_property(name, VM_PROPERTIES);
     size_t size = atol(size_string);
+    int sizeModifier = tolower(size_string[strlen(size_string) - 1]);
+    destroy_property_value(size_string);
+
+    size_t unit = 1;
+    switch (sizeModifier) {
+    case 'k': unit = 1024; break;
+    case 'm': unit = 1024 * 1024; break;
+    case 'g': unit = 1024 * 1024 * 1024;break;
+    }
+
     size_t res = size * unit;
     if (res / unit != size) {
-        // overflow happened
+        /* overflow happened */
         return 0;
     }
     return res;
 }
 
-static bool get_property_value_boolean(char* name, bool deflt) {
-    const char* value = vm_get_property_value(name);
-    if (value == NULL || 0 == value[0])
-       return deflt;
-
-    return (strcmp("0", value) != 0
-        && strcmp("off", value) != 0 
-        && strcmp("false", value) != 0);
-}
-
-static int get_property_value_int(char* name) {
-    const char* value = vm_get_property_value(name);
-    return (NULL == value) ? 0 : atoi(value);
-}
-
-static bool is_property_set(char* name) {
-    const char* value = vm_get_property_value(name);
-    return (NULL != value && 0 != value[0]);
-}
-
 static void parse_configuration_properties() {
     max_heap_size = HEAP_SIZE_DEFAULT;
     min_heap_size = 16 MB;
-    if (is_property_set("gc.mx")) {
-        max_heap_size = parse_size_string(vm_get_property_value("gc.mx"));
+    if (is_property_set("gc.mx", VM_PROPERTIES) == 1) {
+        max_heap_size = get_size_property("gc.mx");
 
         if (max_heap_size < 16 MB) {
             INFO("max heap size is too small: " << max_heap_size);
@@ -123,8 +106,8 @@ static void parse_configuration_properties() {
         if (min_heap_size < 16 MB) min_heap_size = 16 MB;
     }
 
-    if (is_property_set("gc.ms")) {
-        min_heap_size = parse_size_string(vm_get_property_value("gc.ms"));
+    if (is_property_set("gc.ms", VM_PROPERTIES) == 1) {
+        min_heap_size = get_size_property("gc.ms");
 
         if (min_heap_size < 16 MB) {
             INFO("min heap size is too small: " << min_heap_size);
@@ -152,35 +135,36 @@ static void parse_configuration_properties() {
 #endif
 
 
-    if (is_property_set("gc.lp")) {
-        lp_hint = vm_get_property_value("gc.lp");
+    if (is_property_set("gc.lp", VM_PROPERTIES) == 1) {
+        char* value = get_property("gc.lp", VM_PROPERTIES);
+        lp_hint = strdup(value);
+        destroy_property_value(value);
     }
     
-    if (is_property_set("gc.type"))
-        gc_algorithm = get_property_value_int("gc.type");
+    gc_algorithm = get_int_property("gc.type", 0, VM_PROPERTIES);
 
     // version
     INFO(gc_version_string());
     INFO("GC type = " << gc_algorithm);
 
-    if (get_property_value_boolean("gc.ignore_finalizers", false)) {
+    if (get_boolean_property("gc.ignore_finalizers", false, VM_PROPERTIES)) {
         ignore_finalizers = true;
         INFO("GC will ignore finalizers");
     }
 
-    if (get_property_value_boolean("gc.adaptive", true)) {
+    if (get_boolean_property("gc.adaptive", true, VM_PROPERTIES)) {
         INFO("GC will use adaptive algorithm selection");
     } else {
         INFO("GC will NOT use adaptive algorithm selection");
         gc_adaptive = false;
     }
 
-    if (get_property_value_boolean("gc.remember_root_set", false)) {
+    if (get_boolean_property("gc.remember_root_set", false, VM_PROPERTIES)) {
         remember_root_set = true;
         INFO("GC will retrieve root set before any modification in heap");
     }
 
-    if (get_property_value_boolean("gc.heap_iteration", false)) {
+    if (get_boolean_property("gc.heap_iteration", false, VM_PROPERTIES)) {
         jvmti_heap_iteration = true;
         INFO("GC jvmti heap iteration enabled");
     }
@@ -189,7 +173,7 @@ static void parse_configuration_properties() {
 void gc_vm_initialized() {
     TRACE2("gc.init", "gc_vm_initialized");
 
-    if (get_property_value_boolean("gc.heap_iteration", false)) {
+    if (get_boolean_property("gc.heap_iteration", false, VM_PROPERTIES)) {
         jvmti_heap_iteration = true;
         INFO("GC jvmti heap iteration enabled");
     }

vm/gc_gen/src/common/gc_for_vm.cpp
@@ -35,18 +35,20 @@ extern unsigned int NOS_SIZE;
 unsigned int min_heap_size_bytes = 32 * MB;
 unsigned int max_heap_size_bytes = 256 * MB;
 
-static size_t parse_size_string(const char* size_string) 
+static size_t get_size_property(const char* name) 
 {
-  size_t len = strlen(size_string);
-  size_t unit = 1;
-  if (tolower(size_string[len - 1]) == 'k') {
-    unit = 1024;
-  } else if (tolower(size_string[len - 1]) == 'm') {
-    unit = 1024 * 1024;
-  } else if (tolower(size_string[len - 1]) == 'g') {
-    unit = 1024 * 1024 * 1024;
-  }
+  char* size_string = get_property(name, VM_PROPERTIES);
   size_t size = atol(size_string);
+  int sizeModifier = tolower(size_string[strlen(size_string) - 1]);
+  destroy_property_value(size_string);
+
+  size_t unit = 1;
+  switch (sizeModifier) {
+  case 'k': unit = 1024; break;
+  case 'm': unit = 1024 * 1024; break;
+  case 'g': unit = 1024 * 1024 * 1024;break;
+  }
+
   size_t res = size * unit;
   if (res / unit != size) {
     /* overflow happened */
@@ -55,34 +57,13 @@ static size_t parse_size_string(const char* size_string)
   return res;
 }
 
-static bool get_property_value_boolean(char* name) 
-{
-  const char* value = vm_get_property_value(name);
-  
-  return (strcmp("0", value) != 0
-    && strcmp("off", value) != 0 
-    && strcmp("false", value) != 0);
-}
-
-static int get_property_value_int(char* name) 
-{
-  const char* value = vm_get_property_value(name);
-  return (NULL == value) ? 0 : atoi(value);
-}
-
-static bool is_property_set(char* name) 
-{
-  const char* value = vm_get_property_value(name);
-  return (NULL != value && 0 != value[0]);
-}
-
 static void parse_configuration_properties() 
 {
   unsigned int max_heap_size = HEAP_SIZE_DEFAULT;
   unsigned int min_heap_size = min_heap_size_bytes;
   
-  if (is_property_set("gc.mx")) {
-    max_heap_size = parse_size_string(vm_get_property_value("gc.mx"));
+  if (is_property_set("gc.mx", VM_PROPERTIES) == 1) {
+    max_heap_size = get_size_property("gc.mx");
 
     if (max_heap_size < min_heap_size)
       max_heap_size = min_heap_size;
@@ -93,8 +74,8 @@ static void parse_configuration_properties()
     if (min_heap_size < min_heap_size_bytes) min_heap_size = min_heap_size_bytes;
   }
 
-  if (is_property_set("gc.ms")) {
-    min_heap_size = parse_size_string(vm_get_property_value("gc.ms"));
+  if (is_property_set("gc.ms", VM_PROPERTIES) == 1) {
+    min_heap_size = get_size_property("gc.ms");
     if (min_heap_size < min_heap_size_bytes) 
       min_heap_size = min_heap_size_bytes;
   }
@@ -105,22 +86,13 @@ static void parse_configuration_properties()
   min_heap_size_bytes = min_heap_size;
   max_heap_size_bytes = max_heap_size;
 
-  if (is_property_set("gc.nos_size")) {
-    NOS_SIZE = parse_size_string(vm_get_property_value("gc.nos_size"));
+  if (is_property_set("gc.nos_size", VM_PROPERTIES) == 1) {
+    NOS_SIZE = get_size_property("gc.nos_size");
   }
 
-  if (is_property_set("gc.num_collectors")) {
-    unsigned int num = get_property_value_int("gc.num_collectors");
-    NUM_COLLECTORS = (num==0)? NUM_COLLECTORS:num;
-  }
-
-  if (is_property_set("gc.gen_mode")) {
-    NEED_BARRIER = get_property_value_boolean("gc.gen_mode");
-  }
-
-  if (is_property_set("gc.verify")) {
-    GC_VERIFY = get_property_value_boolean("gc.verify");
-  }
+  NUM_COLLECTORS = get_int_property("gc.num_collectors", NUM_COLLECTORS, VM_PROPERTIES);
+  NEED_BARRIER = get_boolean_property("gc.gen_mode", TRUE, VM_PROPERTIES);
+  GC_VERIFY = get_boolean_property("gc.verify", FALSE, VM_PROPERTIES);
   
   return;  
 }

commit 293c732d5b99545ccc535f70aa681e11ff53a7de

Fixed HUT regression since r480913. Bad "java.ext.dirs" value was provided. Tested on SUSE9
git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@481188 13f79535-47bb-0310-9956-ffa450edef68

vm/vmcore/src/init/vm_properties.cpp
@@ -149,7 +149,14 @@ static void init_java_properties(Properties & properties)
     if (NULL == p)
         DIE("Failed to determine executable parent directory");
     *p = '\0';
-    properties.set("java.home", base_path_buf);
+    // home directory
+    char* home_path = apr_pstrdup(prop_pool, base_path_buf);
+    p = strrchr(home_path, PORT_FILE_SEPARATOR);
+    if (NULL == p)
+        DIE("Failed to determine java home directory");
+    *p = '\0';
+
+    properties.set("java.home", home_path);
     properties.set("java.vm.specification.version", "1.0");
     properties.set("java.vm.specification.vendor", "Sun Microsystems Inc.");
     properties.set("java.vm.specification.name", "Java Virtual Machine Specification");
@@ -174,7 +181,7 @@ static void init_java_properties(Properties & properties)
     }
     properties.set("java.library.path", lib_path);
     //java.ext.dirs initialization.
-    char *ext_path = port_filepath_merge(base_path_buf, "lib" PORT_FILE_SEPARATOR_STR "ext", prop_pool);
+    char *ext_path = port_filepath_merge(home_path, "lib" PORT_FILE_SEPARATOR_STR "ext", prop_pool);
     properties.set("java.ext.dirs", ext_path);
     properties.set("os.name", os_name);
     properties.set("os.arch", port_CPU_architecture());