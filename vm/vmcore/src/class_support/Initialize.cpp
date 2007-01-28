1c35aef88e101218f3e5a46ecdb5868aaf95ad60
Geir Magnusson Jr <geirm@apache.org>
2007-01-28 01:42:20 +0000

    added newline to kick CC
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@500727 13f79535-47bb-0310-9956-ffa450edef68

--- vm/vmcore/src/class_support/Initialize.cpp
+++ vm/vmcore/src/class_support/Initialize.cpp
@@ -14,6 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 /** 
  * @author Pavel Pervov
  * @version $Revision: 1.1.2.3.4.4 $
