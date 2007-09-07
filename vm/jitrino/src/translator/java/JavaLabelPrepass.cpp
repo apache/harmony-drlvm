apache/harmony-drlvm

commit ad1b211bb849634a006ba7a514a8bda6b7665d97

Fix for HARMONY-4747 [drlvm][jit][opt] Fixes for escape analysis pass to support unresolved types.
git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@573295 13f79535-47bb-0310-9956-ffa450edef68

vm/jitrino/src/translator/java/JavaLabelPrepass.cpp
@@ -1411,20 +1411,12 @@ Type* JavaLabelPrepass::getTypeByDescriptorString(CompilationInterface& ci, Clas
         len++;
     }
 
-    bool arrayIsWrapped = false;
     TypeManager& typeManager = ci.getTypeManager();
     switch( *descriptorString ) 
     {
     case 'L': 
         {
-            //TODO: check by name if type is already resolved!!!
-            if (!typeManager.isLazyResolutionMode()) {
-                resType = typeManager.getUnresolvedObjectType();
-            } else {
-                resType = ci.getTypeFromDescriptor(enclClass, descriptorString);
-                //in lazy resolution mode retType is already valid array type
-                arrayIsWrapped = true;
-            }
+            resType = ci.getTypeFromDescriptor(enclClass, descriptorString);
             while( *(++descriptorString) != ';' ) {
                 len++;
                 assert(*descriptorString);
@@ -1469,7 +1461,7 @@ Type* JavaLabelPrepass::getTypeByDescriptorString(CompilationInterface& ci, Clas
     }
     assert(resType);
 
-    if (!arrayIsWrapped && arrayDim > 0) {
+    if (arrayDim > 0) {
         for (;arrayDim > 0; arrayDim--) {
             resType = typeManager.getArrayType(resType, false);
         }
@@ -1879,7 +1871,7 @@ void StateTable::mergeSlots(StateInfo::SlotInfo* inSlot, StateInfo::SlotInfo* sl
         if (type) prepass.getVisited()->setBit(offset,false);
         return;
     }
-
+    
     Type* new_type = typeManager.getCommonType(in_type,type);
     if (new_type != NULL) {
         if (vars) {

vm/include/open/vm.h
@@ -823,6 +823,11 @@ VMEXPORT Boolean type_info_is_vector(Type_Info_Handle tih);
 VMEXPORT Boolean type_info_is_general_array(Type_Info_Handle tih);
 
 /**
+* Get the name of the class referenced by this type info handle
+*/
+VMEXPORT const char* type_info_get_type_name(Type_Info_Handle tih);
+
+/**
  * Get the class if <code>type_info_is_reference</code> or 
  * <code>type_info_is_unboxed</code> returned <code>TRUE</code>. 
  * If the type info is a vector or a general array, return the

vm/jitrino/src/jet/cg.h
@@ -1027,12 +1027,6 @@ public:
         }
     }
 
-    /**
-    * @brief Checks if the class name is vmmagic class
-    * @return  - true if the class name is vmmagic class
-    */
-    static bool is_magic_class(const char* kname);
-
 protected:
     /**
      * @brief Tests whether the specified flag is set in method's compilation

vm/jitrino/src/jet/cg_fld_arr.cpp
@@ -27,6 +27,7 @@
 #include "cg.h"
 #include <open/vm.h>
 #include "trace.h"
+#include "VMMagic.h"
 
 namespace Jitrino {
 namespace Jet {
@@ -280,7 +281,7 @@ void CodeGen::do_field_op(const FieldOpInfo& fieldOp)
     jtype jt = to_jtype(class_get_cp_field_type(fieldOp.enclClass, fieldOp.cpIndex));
     
     const char* fieldDescName = const_pool_get_field_descriptor(fieldOp.enclClass, fieldOp.cpIndex);
-    bool fieldIsMagic = is_magic_class(fieldDescName);
+    bool fieldIsMagic = VMMagicUtils::isVMMagicClass(fieldDescName);
     if (fieldIsMagic) {
         jt = iplatf;
     }

vm/jitrino/src/jet/magics.cpp
@@ -30,6 +30,7 @@
 #include "open/vm.h"
 #include "jit_import.h"
 #include "jit_intf.h"
+#include "VMMagic.h"
 
 #include <vector>
 using std::vector;
@@ -37,17 +38,6 @@ using std::vector;
 namespace Jitrino {
 namespace Jet {
 
-bool CodeGen::is_magic_class(const char* kname)
-{
-    static const char unboxedName[] = "org/vmmagic/unboxed/";
-    static const unsigned nameLen = sizeof(unboxedName)-1;
-
-    assert(kname!=NULL);
-    if (*kname=='L') {
-        return !strncmp(kname+1, unboxedName, nameLen);
-    }
-    return !strncmp(kname, unboxedName, nameLen);
-}
 
 static size_t sizeof_jt(jtype jt) {
     static size_t sizes[] =  { 
@@ -107,7 +97,7 @@ bool Compiler::gen_magic(void)
     
     const char* kname = const_pool_get_method_class_name(m_klass, (unsigned short)jinst.op0);
 
-    if (!is_magic_class(kname)) {
+    if (!VMMagicUtils::isVMMagicClass(kname)) {
         return false;
     }
     // This is a magic -> transform it

vm/jitrino/src/optimizer/Inst.cpp
@@ -2127,26 +2127,27 @@ Inst* InstFactory::makeTauLdElem(Modifier mod, Type* type, Opnd* dst, Opnd* arra
                         tauBaseNonNull, tauAddressInRange, type);
 }
 
+#ifdef BRM_CHECK_TYPES
+static void doBRMCheck(Opnd* src, Opnd* dst) {
+    Type *srcType = src->getType();
+    Type *dstType = dst->getType();
+    assert((srcType == dstType) || (srcType->isNullObject() && dstType->isObject()) 
+        || (srcType->isObject() && dstType->isObject() 
+            && (srcType->isUnresolvedType() || dstType->isUnresolvedType()
+                || srcType->asObjectType()->isSubClassOf(dstType->asObjectType()))));
+}
+#endif
+
 Inst* InstFactory::makeLdVar(Opnd* dst, VarOpnd* var) {
 #ifdef BRM_CHECK_TYPES
-    Type *srcType = var->getType();
-    Type *dstType = dst->getType();
-    assert((srcType == dstType) ||
-           (srcType->isNullObject() && dstType->isObject()) ||
-           (srcType->isObject() && dstType->isObject() &&
-            ((ObjectType *)srcType)->isSubClassOf((ObjectType *)dstType)));
+    doBRMCheck(var, dst);
 #endif
     return makeVarAccessInst(Op_LdVar, dst->getType()->tag, dst, var);
 }
 
 Inst* InstFactory::makeLdVar(Opnd* dst, SsaVarOpnd* var) {
 #ifdef BRM_CHECK_TYPES
-    Type *srcType = var->getType();
-    Type *dstType = dst->getType();
-    assert((srcType == dstType) ||
-           (srcType->isNullObject() && dstType->isObject()) ||
-           (srcType->isObject() && dstType->isObject() &&
-            ((ObjectType *)srcType)->isSubClassOf((ObjectType *)dstType)));
+    doBRMCheck(var, dst);
 #endif
     return makeVarAccessInst(Op_LdVar, dst->getType()->tag, dst, var);
 }
@@ -2345,24 +2346,14 @@ Inst* InstFactory::makeTauStElem(Modifier mod, Type::Tag type, Opnd* src,
 
 Inst* InstFactory::makeStVar(VarOpnd* var, Opnd* src) {
 #ifdef BRM_CHECK_TYPES
-    Type *srcType = src->getType();
-    Type *dstType = var->getType();
-    assert((srcType == dstType) ||
-           (srcType->isNullObject() && dstType->isObject()) ||
-           (srcType->isObject() && dstType->isObject() &&
-            ((ObjectType *)srcType)->isSubClassOf((ObjectType *)dstType)));
+    doBRMCheck(src, var);
 #endif
     return makeVarAccessInst(Op_StVar, src->getType()->tag, var, src);
 }
 
 Inst* InstFactory::makeStVar(SsaVarOpnd* var, Opnd* src) {
 #ifdef BRM_CHECK_TYPES
-    Type *srcType = src->getType();
-    Type *dstType = var->getType();
-    assert((srcType == dstType) ||
-           (srcType->isNullObject() && dstType->isObject()) ||
-           (srcType->isObject() && dstType->isObject() &&
-            ((ObjectType *)srcType)->isSubClassOf((ObjectType *)dstType)));
+    doBRMCheck(src, var);
 #endif
     return makeVarAccessInst(Op_StVar, src->getType()->tag, var, src);
 }

vm/jitrino/src/optimizer/escanalyzer.h
@@ -73,24 +73,26 @@ private:
 
     // Connection Graph Structure
 // CnG node types
-    static const uint32 NT_OBJECT   = 8;  // Op_LdRef,Op_NewObj,Op_NewArray,Op_NewMultiArray    
-    static const uint32 NT_DEFARG   = NT_OBJECT+1;  // formal parameter - Op_DefArg
-    static const uint32 NT_RETVAL   = 16;           // Op_DirectCall,Op_IndirectMemoryCall-returned by method 
-    static const uint32 NT_CATCHVAL = NT_RETVAL+1;  // catched value
-    static const uint32 NT_LDOBJ    = 32;           // Op_LdConstant,Op_TauLdInd,Op_LdVar, 
+    enum CnGNodeTypes {
+        NT_OBJECT   = 8,  // Op_LdRef,Op_NewObj,Op_NewArray,Op_NewMultiArray    
+        NT_DEFARG   = NT_OBJECT+1,  // formal parameter - Op_DefArg
+        NT_RETVAL   = 16,           // Op_DirectCall,Op_IndirectMemoryCall-returned by method 
+        NT_CATCHVAL = NT_RETVAL+1,  // catched value
+        NT_LDOBJ    = 32,           // Op_LdConstant,Op_TauLdInd,Op_LdVar, 
                                                     // Op_TauStaticCast,Op_TauCast 
-    static const uint32 NT_INTPTR   = NT_LDOBJ+1;   // Op_SaveRet
-    static const uint32 NT_VARVAL   = NT_LDOBJ+2;   // Op_StVar,Op_Phi
-    static const uint32 NT_ARRELEM  = NT_LDOBJ+3;   // Op_LdArrayBaseAddr,Op_AddScaledIndex
-    static const uint32 NT_REF      = NT_LDOBJ+4;   // reference value - Op_LdFieldAddr,
+        NT_INTPTR   = NT_LDOBJ+1,   // Op_SaveRet
+        NT_VARVAL   = NT_LDOBJ+2,   // Op_StVar,Op_Phi
+        NT_ARRELEM  = NT_LDOBJ+3,   // Op_LdArrayBaseAddr,Op_AddScaledIndex
+        NT_REF      = NT_LDOBJ+4,   // reference value - Op_LdFieldAddr,
                                                     // Op_LdStaticAddr, Op_TauCast, Op_TauStaticCast
-    static const uint32 NT_STFLD    = 64;           // Op_LdStaticAddr
-    static const uint32 NT_INSTFLD  = NT_STFLD+1;   // Op_LdFieldAddr
-    static const uint32 NT_ACTARG   = 128;          // Op_DirectCall,Op_IndirectMemoryCall
-    static const uint32 NT_EXITVAL  = 256;          // returned value - Op_Return
-    static const uint32 NT_THRVAL   = NT_EXITVAL+1; // thrown value - Op_Throw
-    static const uint32 NT_LDVAL    = 512;          // Op_TauLdInd, Op_TauStInd
-	static const uint32 NT_OBJS     = NT_OBJECT|NT_RETVAL|NT_LDOBJ;  //for findCnGNode_op
+        NT_STFLD    = 64,           // Op_LdStaticAddr
+        NT_INSTFLD  = NT_STFLD+1,   // Op_LdFieldAddr
+        NT_ACTARG   = 128,          // Op_DirectCall,Op_IndirectMemoryCall
+        NT_EXITVAL  = 256,          // returned value - Op_Return
+        NT_THRVAL   = NT_EXITVAL+1, // thrown value - Op_Throw
+        NT_LDVAL    = 512,          // Op_TauLdInd, Op_TauStInd
+        NT_OBJS     = NT_OBJECT|NT_RETVAL|NT_LDOBJ  //for findCnGNode_op
+    };
 // CnG node reference types
     static const uint32 NR_PRIM = 0;
     static const uint32 NR_REF = 1;
@@ -245,22 +247,8 @@ private:
     // list of Op_Branch and Op_TauCheckNull instructions (for scalar replacement optimization)
     Insts* checkInsts;
 
-    // variable used to output debug information
-    int _cfgirun;
-    int _instrInfo;
-    int _instrInfo2;
-    int _cngnodes;
-    int _cngedges;
-    int _scanMtds;
-    int _setState;
-    int _printstat;
-    int _eainfo;
-    int _seinfo;
-    int _scinfo;
-#define  prsNum  10
-    int prsArr[prsNum];
-
-
+    bool shortLog;
+    bool verboseLog;
     // common method for both EscAnalyzer constructors
     void init();
     // output escape analysis flags
@@ -299,12 +287,12 @@ private:
 /**
  * Creates new CnG node for specified call instruction argument. 
  * @param inst - instruction,
- * @param md - callee method description,
+ * @param mpt - callee method description,
  * @param ntype - CnG node type,
  * @param narg - method argument.
  * @return created CnGnode. 
  */
-    CnGNode* addCnGNode_mp(Inst* inst, MethodDesc* md, uint32 ntype, uint32 narg);
+    CnGNode* addCnGNode_mp(Inst* inst, MethodPtrType* mpt, uint32 ntype, uint32 narg);
 
 /**
  * Creates new CnG node for specified return or throw instruction. 
@@ -1003,6 +991,12 @@ private:
  * @param scObjFlds - list to collect onode operand field usage.
  */
     void checkToScalarizeFinalFiels(CnGNode* onode, ScObjFlds* scObjFlds);
+
+
+    void instrExam_processLdFieldAddr(Inst* inst);
+    void instrExam2_processLdFieldAddr(Inst* inst);
+    static void getLdFieldAddrInfo(Inst* inst, Type*& type, uint32& nType);
+
 };
 
 } //namespace Jitrino 

commit 18e7162b08ab4f3206416e0fe54b91f44cdea4d1

Fix for regression introduced by r573295 (HARMONY-4747 patch)
git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@573498 13f79535-47bb-0310-9956-ffa450edef68

vm/jitrino/src/translator/java/JavaLabelPrepass.cpp
@@ -1416,7 +1416,11 @@ Type* JavaLabelPrepass::getTypeByDescriptorString(CompilationInterface& ci, Clas
     {
     case 'L': 
         {
-            resType = ci.getTypeFromDescriptor(enclClass, descriptorString);
+            if (!typeManager.isLazyResolutionMode()) {
+                resType = typeManager.getUnresolvedObjectType();
+            } else {
+                resType = ci.getTypeFromDescriptor(enclClass, descriptorString);
+            }
             while( *(++descriptorString) != ';' ) {
                 len++;
                 assert(*descriptorString);