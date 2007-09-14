35ee871395efa2cb3223947aad3456d713b8cee9
Alexey Varlamov <varlax@apache.org>
2007-09-14 12:04:09 +0000

    Reg test to HARMONY-4675
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@575649 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/regression/H4675/DivLongTest.java
@@ -0,0 +1,30 @@
+package org.apache.harmony.drlvm.tests.regression.h4675;
+
+import junit.framework.TestCase;
+
+public class DivLongTest extends TestCase {
+
+    public void testLDIV_lowzero() {
+        long test_long = 0x000000ff00000000L; 
+        long res = test_long / test_long; 
+
+        assertEquals(1, res);
+    }
+
+    /*  crash on x86_64 - to be fixed
+    public void testLDIV_min() {
+        long l_min = Long.MIN_VALUE;
+        long l_1 = -1;
+        long res = l_min / l_1;
+
+        assertEquals(Long.MIN_VALUE, res);
+    }
+
+    public void testLREM_min() {
+        long l_min = Long.MIN_VALUE;
+        long l_1 = -1;
+        long res = l_min % l_1;
+
+        assertEquals(0, res);
+    } */
+}
