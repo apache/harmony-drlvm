5e1e373a3a4205e9184e70603ba1d6f7abb489a7
Gregory Shimansky <gshimansky@apache.org>
2008-03-20 16:56:14 +0000

    Applied patch from HARMONY-5624 [drlvm][port][signals] Crash handler crashes on SIGSEGV processing in native thread
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@639356 13f79535-47bb-0310-9956-ffa450edef68

--- vm/vmcore/src/util/linux/ia32_em64t/signals_common.cpp
+++ vm/vmcore/src/util/linux/ia32_em64t/signals_common.cpp
@@ -353,6 +353,10 @@ Boolean null_reference_handler(port_sigtype UNREF signum, Registers* regs, void*
         regs->set_ip(new_ip);
     }
 
+    if (!vmthread || env == NULL ||
+        !is_in_java(regs) || interpreter_enabled())
+        return FALSE; // Crash
+
     if (check_stack_overflow(regs, fault_addr))
     {
         Boolean result = stack_overflow_handler(signum, regs, fault_addr);
@@ -362,10 +366,6 @@ Boolean null_reference_handler(port_sigtype UNREF signum, Registers* regs, void*
 
         return result;
     }
-
-    if (!vmthread || env == NULL ||
-        !is_in_java(regs) || interpreter_enabled())
-        return FALSE; // Crash
 
     // Pass exception to NCAI exception handler
     bool is_handled = 0;
