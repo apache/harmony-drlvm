fba2eb12851cee59c0b5ce0e7f256d64bf69dd8e
Mark Hindess <hindessm@apache.org>
2008-06-26 14:11:14 +0000

    Applying patches from "[#HARMONY-5693] Redefinition of vm_notify_live_object_class in vm_gc.h".
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/drlvm/trunk@671911 13f79535-47bb-0310-9956-ffa450edef68

--- vm/include/open/vm_gc.h
+++ vm/include/open/vm_gc.h
@@ -109,13 +109,6 @@ VMEXPORT void vm_classloader_iterate_objects(void *iterator);
 VMEXPORT bool vm_iterate_object(Managed_Object_Handle object);
 
 /**
- * GC calls this function for each live object it finds in heap.
- * This is used for finding unreferenced class loaders for class
- * unloading.
- */
-VMEXPORT void vm_notify_live_object_class(Class_Handle);
-
-/**
  * GC calls this function to hint VM that finalizers may need to be run
  * and references enqueued. This method is guaranteed not to hold global
  * GC lock. 
@@ -210,6 +203,9 @@ VMEXPORT void set_native_ref_enqueue_thread_flag(Boolean flag);
 VMEXPORT Class_Handle vtable_get_class(VTable_Handle vh);
 
 /**
+ * GC calls this function for each live object it finds in heap.
+ * This is used for finding unreferenced class loaders for class
+ * unloading.
  * Notifies VM that live object of this class was found in the heap
  *
  * @param clss - class of live object in Java heap
